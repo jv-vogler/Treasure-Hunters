bb8434905226b85fe40f70952f2371b75e28a462
jv-vogler <jvsvogler@gmail.com>
2022-12-05 02:58:21 -0300

    Add attack state to Fierce Tooth enemy

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -4,12 +4,17 @@ extends Entity
 @export var level: int = 1
 @export_range(60, 600, 10) var speed: float = 80.0
 @export_range(1, 9, 1) var stagger_limit: int = 3
-@export var staggerable: bool = true
 @export var loot_table: Array = []
+@export var staggerable: bool = true
 
 var jump_velocity: float = -384.0
-var stagger_count: int = 1
 var target: Player = null
+
+var stagger_count: int = 0:
+	set(value):
+		stagger_count = clampi(value, 0, stagger_limit)
+		if stagger_count == stagger_limit: staggerable = false
+
 var direction: float:
 	get:
 		if target:
@@ -25,6 +30,7 @@ var direction: float:
 		return direction
 
 @onready var player_detection = $PlayerDetection
+@onready var attack_range = $Sprite/AttackRange
 
 
 func _ready() -> void:
---
+++ enemies/fierce_tooth/states/attack.gd
@@ -0,0 +1,56 @@
+extends BaseState
+
+@export var idle_node: NodePath
+@export var hurt_node: NodePath
+@export var dead_node: NodePath
+@export_range(0, 550.0, 50.0) var lunge_distance: float = 350.0
+
+var attack_finished: bool = false
+
+@onready var idle_state: BaseState = get_node(idle_node)
+@onready var hurt_state: BaseState = get_node(hurt_node)
+@onready var dead_state: BaseState = get_node(dead_node)
+@onready var enemy: Enemy = owner
+
+
+func enter() -> void:
+	super.enter()
+	enemy.animations.play("Anticipation")
+	enemy.velocity.x = lerp(enemy.velocity.x, 0.0, 0.9)
+	await enemy.animations.animation_finished
+	if !is_hurt:
+		_attack()
+
+
+func exit() -> void:
+	attack_finished = false
+
+
+func physics_process(delta: float) -> BaseState:
+	if enemy.current_health == 0:
+		return dead_state
+
+	if is_hurt and enemy.staggerable:
+		return hurt_state
+
+	if attack_finished:
+		return idle_state
+
+	enemy.velocity.x = lerp(enemy.velocity.x, 0.0, 0.05)
+	enemy.velocity.y += enemy.gravity * delta
+	enemy.move_and_slide()
+
+	return null
+
+
+func _attack() -> void:
+	enemy.animations.play("Attack")
+	enemy.velocity = Vector2(lunge_distance * enemy.direction, -150.0)
+	await enemy.animations.animation_finished
+	attack_finished = true
+	enemy.staggerable = true
+	enemy.stagger_count = 0
+
+
+func _on_took_damage() -> void:
+	is_hurt = true if enemy.staggerable else false
