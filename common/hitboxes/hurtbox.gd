594b4e66b355ab2f4c4971de8ced878aef5c0a86
jv-vogler <jvsvogler@gmail.com>
2022-12-04 21:48:39 -0300

    Add a debugger scene

--- common/hitboxes/hurtbox.gd
+++ common/hitboxes/hurtbox.gd
@@ -20,10 +20,7 @@ func _on_area_entered(hitbox: Hitbox) -> void:
 		owner.take_damage(hitbox.damage)
 		owner.emit_signal("took_damage")
 
-		var hurtbox_pos: float = get_child(0).global_position.x + owner.global_position.x
-		var hitbox_pos: float = hitbox.get_child(0).global_position.x + hitbox.owner.global_position.x
-
-		var direction: float = hurtbox_pos - hitbox_pos
+		var direction: float = owner.position.x - hitbox.owner.position.x
 		if direction > 0:
 			emit_signal("hit_direction", 1)
 		elif direction < 0:
